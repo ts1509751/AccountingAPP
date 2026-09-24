8fc4d8da4832dc0a2bcaffce27f5ec8e5948d041
NJ <ts1509751@gmail.com>
2026-09-25 03:14:11 +0800

    feat: integrate google-services.json and update firebase config to accountingapp-88ab5

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { Capacitor } from '@capacitor/core';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhGKmhVkhsjA5LtjNrjnbHEzjVRQB5zho",
-  authDomain: "my-expense-tracker-90de8.firebaseapp.com",
-  projectId: "my-expense-tracker-90de8",
-  storageBucket: "my-expense-tracker-90de8.firebasestorage.app",
-  messagingSenderId: "382467613969",
-  appId: "1:382467613969:web:813a097f684858fdc6d8b0"
+  apiKey: "AIzaSyDMRCLa_cJI5SrzbV0BN7HjW_Z-ClNwb9M",
+  authDomain: "accountingapp-88ab5.firebaseapp.com",
+  projectId: "accountingapp-88ab5",
+  storageBucket: "accountingapp-88ab5.firebasestorage.app",
+  messagingSenderId: "456811032968",
+  appId: "1:456811032968:android:f68acfd8386c36d06d4a3d"
 };
 
 const app = initializeApp(firebaseConfig);
